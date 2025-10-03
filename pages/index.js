170c615763c9b60020dee901de3d16595672a65b
Jefferson Eneas da Silva <113453053+jeffersoneneas@users.noreply.github.com>
2025-10-03 20:44:56 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function home(){
-    return <h1>Mylena, eu amo você. Se você me ama, dá uma risadinha!</h1>
+function home() {
+  return <h1>Mylena, eu amo você. Se você me ama, dá uma risadinha!</h1>;
 }
 
 export default home;

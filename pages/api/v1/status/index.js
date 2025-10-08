cf659c44a67bb07c8f2c7bcdb183e2bc6f4f8739
Jefferson Eneas da Silva <113453053+jeffersoneneas@users.noreply.github.com>
2025-10-08 19:43:47 +0000

    adiciona arquivo `domain.yaml`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,12 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum");
+  console.log(result.rows);
   response.status(200).json({
     batata: "doce",
     bife: "parmegiana",
+    nome: "Daniel",
   });
 }
 

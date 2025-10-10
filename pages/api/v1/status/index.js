caf9642d10a80779c149566209131657d4ebfd66
Jefferson Eneas da Silva <113453053+jeffersoneneas@users.noreply.github.com>
2025-10-10 19:35:32 +0000

    move `.env` file to `.env.development`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -4,9 +4,7 @@ async function status(request, response) {
   const result = await database.query("SELECT 1 + 1 as sum");
   console.log(result.rows);
   response.status(200).json({
-    batata: "doce",
-    bife: "parmegiana",
-    nome: "Daniel",
+    nome: "Jefferson",
   });
 }
 
